8320c164ac0e6d664f90f88a292a69848ee1fa39
David Atlan <david@davipro.fr>
2024-01-03 18:05:18 +0100

    Update snap counts in app.component.ts and face-snap.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
             description: 'Mon meilleur ami depuis petit !',
             imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
             createdDate: new Date(),
-            snaps: 0,
+            snaps: 90,
             location: 'Paris'
         },
         {
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit {
             description: 'Un endroit magnifique pour les randonnéees.',
             imageUrl: 'https://cdn.pixabay.com/photo/2014/12/17/18/55/rock-571801_1280.jpg',
             createdDate: new Date(),
-            snaps: 0,
+            snaps: 180,
             location: 'la montagne'
         },
         {
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit {
             description: 'Mmmh que c\'est bon !',
             imageUrl: 'https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg',
             createdDate: new Date(),
-            snaps: 0
+            snaps: 255
         }
     ];
   }
