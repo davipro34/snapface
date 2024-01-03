a711becac0b4057cc1d1ed94ce93e5fb426f3e57
David Atlan <david@davipro.fr>
2024-01-03 14:55:24 +0100

    Add multiple face snaps to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,29 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
 
   ngOnInit() {
     this.mySnap = new FaceSnap(
         'Archibald',
         'Mon meilleur ami depuis petit !',
         'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        0
+    );
+    this.myOtherSnap = new FaceSnap(
+        'Three Rock Mountain',
+        'Un endroit magnifique pour les randonnéees.',
+        'https://cdn.pixabay.com/photo/2014/12/17/18/55/rock-571801_1280.jpg',
+        new Date(),
+        0
+    );
+    this.myLastSnap = new FaceSnap(
+        'Un bon repas',
+        'Mmmh que c\'est bon !',
+        'https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg',
         new Date(),
         0
     );
