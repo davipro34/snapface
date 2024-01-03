f6859ee39ffb84ce66c34b01cc62fa6659f1b773
David Atlan <david@davipro.fr>
2024-01-03 16:42:20 +0100

    Refactor app.component.html and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,33 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis petit !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'Paris'
-    };
-    this.myOtherSnap = {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnéees.',
-        imageUrl: 'https://cdn.pixabay.com/photo/2014/12/17/18/55/rock-571801_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'la montagne'
-    };
-    this.myLastSnap = {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0
-    };
+    this.faceSnaps = [
+        {
+            title: 'Archibald',
+            description: 'Mon meilleur ami depuis petit !',
+            imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+            createdDate: new Date(),
+            snaps: 0,
+            location: 'Paris'
+        },
+        {
+            title: 'Three Rock Mountain',
+            description: 'Un endroit magnifique pour les randonnéees.',
+            imageUrl: 'https://cdn.pixabay.com/photo/2014/12/17/18/55/rock-571801_1280.jpg',
+            createdDate: new Date(),
+            snaps: 0,
+            location: 'la montagne'
+        },
+        {
+            title: 'Un bon repas',
+            description: 'Mmmh que c\'est bon !',
+            imageUrl: 'https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg',
+            createdDate: new Date(),
+            snaps: 0
+        }
+    ];
   }
 }
