476f11178bf867b9d6e37e664e0fd0b1a52ed758
David Atlan <david@davipro.fr>
2024-01-04 09:58:17 +0100

    Refactor app component to use face snap list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -7,33 +6,6 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  faceSnaps!: FaceSnap[];
 
-  ngOnInit() {
-    this.faceSnaps = [
-        {
-            title: 'Archibald',
-            description: 'Mon meilleur ami depuis petit !',
-            imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-            createdDate: new Date(),
-            snaps: 90,
-            location: 'Paris'
-        },
-        {
-            title: 'Three Rock Mountain',
-            description: 'Un endroit magnifique pour les randonnéees.',
-            imageUrl: 'https://cdn.pixabay.com/photo/2014/12/17/18/55/rock-571801_1280.jpg',
-            createdDate: new Date(),
-            snaps: 180,
-            location: 'la montagne'
-        },
-        {
-            title: 'Un bon repas',
-            description: 'Mmmh que c\'est bon !',
-            imageUrl: 'https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg',
-            createdDate: new Date(),
-            snaps: 255
-        }
-    ];
-  }
+  ngOnInit() {}
 }
---
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+
+@Component({
+  selector: 'app-face-snap-list',
+  templateUrl: './face-snap-list.component.html',
+  styleUrl: './face-snap-list.component.scss'
+})
+export class FaceSnapListComponent implements OnInit {
+
+  faceSnaps!: FaceSnap[];
+
+  constructor() { }
+  
+  ngOnInit() {
+    this.faceSnaps = [
+        {
+            title: 'Archibald',
+            description: 'Mon meilleur ami depuis petit !',
+            imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+            createdDate: new Date(),
+            snaps: 90,
+            location: 'Paris'
+        },
+        {
+            title: 'Three Rock Mountain',
+            description: 'Un endroit magnifique pour les randonnéees.',
+            imageUrl: 'https://cdn.pixabay.com/photo/2014/12/17/18/55/rock-571801_1280.jpg',
+            createdDate: new Date(),
+            snaps: 180,
+            location: 'la montagne'
+        },
+        {
+            title: 'Un bon repas',
+            description: 'Mmmh que c\'est bon !',
+            imageUrl: 'https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg',
+            createdDate: new Date(),
+            snaps: 255
+        }
+    ];
+  }
+}
