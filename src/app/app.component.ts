ffd0b1778d702412542ac7605140a0ea14fa4684
David Atlan <david@davipro.fr>
2024-01-05 15:19:23 +0100

    Add interval logic to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, filter, interval, map, tap } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {}
+  interval$!: Observable<string>;
+
+  ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value %3 === 0),
+      map(value => value % 2 === 0 ?
+          `Je suis ${value} et je suis pair` : 
+          `Je suis ${value} et je suis impair`
+        ),
+      tap(text => this.logger(text))
+    );
+  }
+
+  logger(text: string) {
+    console.log(`Log: ${text}`);
+  }
 }
